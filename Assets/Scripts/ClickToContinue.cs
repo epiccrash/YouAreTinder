e2f85843969190fb62731422d6ccf48846049942
jkcao <jkcao@andrew.cmu.edu>
2020-02-02 11:16:34 -0500

    Added game flow * start -> gameplay -> end * win state * updates to timeline UI to be shinier

---
+++ Assets/Scripts/ClickToContinue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToContinue : MonoBehaviour
+{
+    public int SceneIndexToLoad;
+    public float TimeBeforeActive;
+
+    private bool canExit = false;
+
+    private void Start() {
+        StartCoroutine(ActivateTimer());
+    }
+
+    private IEnumerator ActivateTimer() {
+        yield return new WaitForSeconds(TimeBeforeActive);
+        canExit = true;
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0) && canExit) {
+            SceneManager.LoadScene(SceneIndexToLoad);
+        }
+    }
+
+}
