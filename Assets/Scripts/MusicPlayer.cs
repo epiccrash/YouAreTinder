0da0d0772e446aaee774b122d3b963f765f44184
Sartharis <jan.orlowsk@gmail.com>
2020-02-02 12:50:40 -0500

    Musicccc

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : UnitySingletonPersistent<MusicPlayer>
+{
+    float musicTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    private void Update()
+    {
+         DontDestroyOnLoad(this.gameObject);
+        musicTime = GetComponent<AudioSource>().time;
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        GetComponent<AudioSource>().Play();
+        GetComponent<AudioSource>().time = musicTime;
+    }
+
+}
