63f8a7980cda5cd729d620821ed9693fbfa55265
Sartharis <jan.orlowsk@gmail.com>
2020-02-02 13:43:32 -0500

    fixes

---
+++ Assets/Restarter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restarter : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if(Input.GetButton("restart"))
+        {
+
+            GameState.Instance.currentPoints = 0;
+            SceneManager.LoadScene(0);
+        }   
+    }
+}
