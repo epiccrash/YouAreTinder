86bf19497abc75238e56afa5b5f21192bc7d7169
jkcao <jkcao@andrew.cmu.edu>
2020-02-02 12:19:14 -0500

    Matchmaking + game state fixes

--- Assets/Scripts/MatchmakingState.cs
+++ Assets/Scripts/MatchmakingState.cs
@@ -7,13 +7,9 @@ public class MatchmakingState : UnitySingletonPersistent<MatchmakingState>
     public float compatibility = 0;
     public string name1 = "";
     public string name2 = "";
-<<<<<<< HEAD
-    public Dictionary<string, int> sharedPreferences;
-=======
     public Sprite profile1 = null;
     public Sprite profile2 = null;
-    public Dictionary<string, float> sharedPreferences;
->>>>>>> accf35133de968edc9d899522b2993c0dadf73e9
+    public Dictionary<string, int> sharedPreferences;
     //string : preferene name
     //Float: -1 both dislike, 0 one like one dislike, 1 both like
 }
